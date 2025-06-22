c4ec83e7ffdd892c096274f4641028db91e3cc87
darley523 <gac.av@hotmail.com>
2025-06-22 00:02:46 -0400

    Experiencia 3 Testing

--- Exp3/perfulandia_crud/src/main/java/com/perfulandia/perfulandia/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.perfulandia.perfulandia.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.perfulandia.perfulandia.entities.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-
-}
