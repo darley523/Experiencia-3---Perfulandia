20a1f3a4914d801d275db96af6618b885a55207a
darley523 <gac.av@hotmail.com>
2025-06-22 00:12:52 -0400

    Experiencia 3 Testing

--- perfulandia_crud/src/main/java/com/perfulandia/perfulandia/repository/BoletaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.perfulandia.perfulandia.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.perfulandia.perfulandia.entities.Boleta;
-
-public interface BoletaRepository extends CrudRepository<Boleta, Long> {
-
-}
